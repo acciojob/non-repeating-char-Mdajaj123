0cdc2b4ae82e80b0fd28b927e2ab3904cbd7f794
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-06 23:38:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function firstNonRepeatedChar(str) {
 	
  // Write your code here
 	for(let i=0;i<str.length;i++){
-		if(str.firstIndexOf(str[i])==lastIndexOf(str[i]){
+		if(str.firstIndexOf(str[i])==lastIndexOf(str[i])){
 			console.log(str[i]);
 			break;
 		}
