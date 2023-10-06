42ea39630cefdde42810b5cb6efce3190cd96d12
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-06 23:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
+	
  // Write your code here
+	for(let i=0;i<str.length;i++){
+		if(str.firstIndexOf(str.charAt(i))==lastIndexOf(str.charAt(i))){
+			return str.charAt(i);
+		}
+		else{
+			return null;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
