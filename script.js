60b88f4a2167cbdbfdfdf0ef81501357d0823a5b
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-17 17:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,28 +2,44 @@
 // Write, Edit and Run your Javascript code using JS Online Compiler
 
 function firstNonRepeatedChar(str) {
+	let obj={};
+	for(let i=0;i<str.length;i++){
+		if(obj[str[i]]!=undefined){
+			obj[str[i]]+=1;
+		}
+		else{
+			obj[str[i]]=1;
+		}
+	}
+for(let i=0;i<str.length;i++){
+	if(obj[str[i]]==1){
+		return str[i];
+	}
+}
+	return null;
+	
 	
  // Write your code here
-	 const charCount = {};
+	//  const charCount = {};
 
-  // Iterate through the string to count character frequencies
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
+ //  // Iterate through the string to count character frequencies
+ //  for (let i = 0; i < str.length; i++) {
+ //    const char = str[i];
+ //    charCount[char] = (charCount[char] || 0) + 1;
+ //  }
 
-  // Iterate through the string again to find the first non-repeating character
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (charCount[char] === 1) {
-    return char; // Return the first non-repeating character
-    }
-  }
+ //  // Iterate through the string again to find the first non-repeating character
+ //  for (let i = 0; i < str.length; i++) {
+ //    const char = str[i];
+ //    if (charCount[char] === 1) {
+ //    return char; // Return the first non-repeating character
+ //    }
+ //  }
 
-  // If no non-repeating character is found, return null or a default value
- return null;
+ //  // If no non-repeating character is found, return null or a default value
+ // return null;
 }
 	// console.log("null");
 
-const input = prompt("Enter a string");
+const  input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
